8b9784654e1d0264dbf6b375a043ddf8f8ce75a6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-11-18 10:07:38 +0000

    Launcher3: Always calculate qsb width correctly
    Instead of taking up a fixed width, calculate qsb width according
    to the device profile which accounts for grid size etc. This aligns
    the qsb with the hotseat icons above it.

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -845,8 +845,7 @@ public class DeviceProfile {
                     - hotseatBorderSpace * numShownHotseatIcons
                     - iconExtraSpacePx;
         } else {
-            return getIconToIconWidthForColumns(mHotseatColumnSpan) - iconExtraSpacePx
-                    - hotseatBorderSpace;
+            return getIconToIconWidthForColumns(mHotseatColumnSpan) - iconExtraSpacePx;
         }
     }
 
@@ -915,8 +914,6 @@ public class DeviceProfile {
      * necessary.
      */
     public void recalculateHotseatWidthAndBorderSpace() {
-        if (!(mIsScalableGrid || (isTaskbarPresent && !isLandscape))) return;
-
         updateHotseatWidthAndBorderSpace(inv.numColumns);
         int numWorkspaceColumns = getPanelCount() * inv.numColumns;
         if (isTwoPanels) {
